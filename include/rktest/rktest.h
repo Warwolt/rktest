0945cf837763b306bd8e997cde6fd2e8aafd049f
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-11-27 16:20:21 +0100

    Parse memory section into test suites
    Add code that goes through the `rktest` memory section and registers
    tests into test suites and test cases.
    
    - Rename struct to rktest_test_meta_t, remove _ prefix on functions
    - Make result an enum
    - Add rktest_printf_test_step macro
    - Add foreach macro
    - Add suites and test to suites

--- include/rktest/rktest.h
+++ include/rktest/rktest.h
@@ -1,23 +1,57 @@
+/*
+------------------------------------------------------------------------------
+Public Domain (www.unlicense.org)
+This is free and unencumbered software released into the public domain.
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this
+software, either in source code form or as a compiled binary, for any purpose,
+commercial or non-commercial, and by any means.
+In jurisdictions that recognize copyright laws, the author or authors of this
+software dedicate any and all copyright interest in the software to the public
+domain. We make this dedication for the benefit of the public at large and to
+the detriment of our heirs and successors. We intend this dedication to be an
+overt act of relinquishment in perpetuity of all present and future rights to
+this software under copyright law.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------
+*/
+
 #include <stdbool.h>
 #include <stddef.h>
 
 #ifndef RKTEST_H
 #define RKTEST_H
 
-// RKTest is a small unit test library for C99 heavily based on Google Test.
+// RKTest is a small unit test library for C99 with an interface heavily based on Google Test.
 
-/* Public API -------------------------------------------------------------------------------------*/
-#define TEST(SUITE, NAME)                                                                                                   \
-	__pragma(data_seg(push));                                                                                               \
-	__pragma(section("rktest$data", read));                                                                                 \
-	void SUITE##_##NAME##_impl(void);                                                                                       \
-	const _rktest_test_t SUITE##_##NAME##_data = {                                                                          \
-		.name = #NAME,                                                                                                      \
-		.suite = #SUITE,                                                                                                    \
-		.func = &SUITE##_##NAME##_impl                                                                                      \
-	};                                                                                                                      \
-	__declspec(allocate("rktest$data")) const _rktest_test_t* const SUITE##_##NAME##_data##_##ptr = &SUITE##_##NAME##_data; \
-	__pragma(data_seg(pop));                                                                                                \
+/* Config variables --------------------------------------------------------- */
+// Define these variables before including rktest.h to configure the max number
+// of supported test suites and max number of unit tests per suite.
+
+#ifndef RKTEST_MAX_NUM_TEST_SUITES
+#define RKTEST_MAX_NUM_TEST_SUITES 512
+#endif
+
+#ifndef RKTEST_MAX_NUM_TESTS_PER_SUITE
+#define RKTEST_MAX_NUM_TESTS_PER_SUITE 64
+#endif
+
+/* Public API --------------------------------------------------------------- */
+#define TEST(SUITE, NAME)                                                                                                        \
+	__pragma(data_seg(push));                                                                                                    \
+	__pragma(section("rktest$data", read));                                                                                      \
+	void SUITE##_##NAME##_impl(void);                                                                                            \
+	const rktest_macro_data_t SUITE##_##NAME##_meta = {                                                                          \
+		.test_name = #NAME,                                                                                                      \
+		.suite_name = #SUITE,                                                                                                    \
+		.func = &SUITE##_##NAME##_impl                                                                                           \
+	};                                                                                                                           \
+	__declspec(allocate("rktest$data")) const rktest_macro_data_t* const SUITE##_##NAME##_meta##_##ptr = &SUITE##_##NAME##_meta; \
+	__pragma(data_seg(pop));                                                                                                     \
 	void SUITE##_##NAME##_impl(void)
 
 #define EXPECT_EQ(lhs, rhs)
@@ -27,37 +61,44 @@
 
 int rktest_main(void);
 
-/* Test runner internals -------------------------------------------------------------------------*/
-typedef int _rktest_result_t;
-#define _RKTEST_RESULT_OK ((_rktest_result_t)0)
-#define _RKTEST_RESULT_ERROR ((_rktest_result_t)1)
-
+/* Test runner internals ---------------------------------------------------- */
+/**
+ * Collects all the information from a TEST() macro
+ *
+ * Instances of the struct are stored locally in the unit test files. Pointers
+ * to instances of this struct are collected and stored in the `rktest$data`
+ * memory section.
+ */
 typedef struct {
-	const char* name;
-	const char* suite;
+	const char* test_name;
+	const char* suite_name;
 	void (*func)(void);
-} _rktest_test_t;
+} rktest_macro_data_t;
 
-inline bool _rktest_colors_enabled(void);
+inline bool rktest_colors_enabled(void);
 
-#define _RKTEST_COLOR_GREEN (_rktest_colors_enabled() ? "\033[32m" : "")
-#define _RKTEST_COLOR_RED (_rktest_colors_enabled() ? "\033[31m" : "")
-#define _RKTEST_COLOR_YELLOW (_rktest_colors_enabled() ? "\033[33m" : "")
-#define _RKTEST_COLOR_RESET (_rktest_colors_enabled() ? "\033[0m" : "")
+#define RKTEST_COLOR_GREEN (rktest_colors_enabled() ? "\033[32m" : "")
+#define RKTEST_COLOR_RED (rktest_colors_enabled() ? "\033[31m" : "")
+#define RKTEST_COLOR_YELLOW (rktest_colors_enabled() ? "\033[33m" : "")
+#define RKTEST_COLOR_RESET (rktest_colors_enabled() ? "\033[0m" : "")
 
-#define _rktest_printf_green(...)      \
-	printf("%s", _RKTEST_COLOR_GREEN); \
+#define rktest_printf_green(...)      \
+	printf("%s", RKTEST_COLOR_GREEN); \
+	printf(__VA_ARGS__);              \
+	printf("%s", RKTEST_COLOR_RESET)
+
+#define rktest_printf_red(...)      \
+	printf("%s", RKTEST_COLOR_RED); \
+	printf(__VA_ARGS__);            \
+	printf("%s", RKTEST_COLOR_RESET)
+
+#define rktest_printf_yellow(...)      \
+	printf("%s", RKTEST_COLOR_YELLOW); \
 	printf(__VA_ARGS__);               \
-	printf("%s", _RKTEST_COLOR_RESET)
+	printf("%s", RKTEST_COLOR_RESET)
 
-#define _rktest_printf_red(...)      \
-	printf("%s", _RKTEST_COLOR_RED); \
-	printf(__VA_ARGS__);             \
-	printf("%s", _RKTEST_COLOR_RESET)
-
-#define _rktest_printf_yellow(...)      \
-	printf("%s", _RKTEST_COLOR_YELLOW); \
-	printf(__VA_ARGS__);                \
-	printf("%s", _RKTEST_COLOR_RESET)
+#define rktest_printf_test_step(prefix_str, ...) \
+	rktest_printf_green(prefix_str);             \
+	printf(__VA_ARGS__);
 
 #endif /* RKTEST_H */
--- src/rktest.c
+++ src/rktest.c
@@ -1,117 +1,189 @@
 #include "rktest/rktest.h"
 
+#include <memory.h>
 #include <stdio.h>
+#include <string.h>
+
 #ifdef WIN32
 #include <windows.h>
 #endif
 
+/* RK Test runner internals ------------------------------------------------- */
+#define RKTEST_MAX_NUM_TESTS (RKTEST_MAX_NUM_TEST_SUITES * RKTEST_MAX_NUM_TESTS_PER_SUITE)
+
+#define foreach(type_ptr, iter, array, array_len) \
+	for (type_ptr iter = &array[0]; iter != &array[array_len]; iter++)
+
+typedef enum {
+	RKTEST_RESULT_OK,
+	RKTEST_RESULT_ERROR,
+} rktest_result_t;
+
+typedef struct {
+	const char* name;
+	void (*func)(void);
+} rktest_test_t;
+
+typedef struct {
+	const char* name;
+	rktest_test_t tests[RKTEST_MAX_NUM_TESTS_PER_SUITE];
+	size_t num_tests;
+} rktest_suite_t;
+
 // Unit test case storage
 //
-// An `rktest` data section is added to the binary, and is used to store meta data about all unit
-// test cases to be run. Three fragments are added: `begin`, `data`, and `end`. The names utilize
-// that fragments are sorted and stored alphabetically in the binary.
+// An `rktest` data section is added to the binary, and is used to store meta
+// data about all unit test cases to be run. Three fragments are added: `begin`,
+// `data`, and `end`. The names utilize that fragments are sorted and stored
+// alphabetically in the binary.
 //
-// All unit test files adds local test data to the `rktest$data` fragment of the section with the
-// TEST() macro, and the linker will then collect them into a single `rktest$data` fragment in the
-// binary.
+// All unit test files adds local test data to the `rktest$data` fragment of the
+// section with the TEST() macro, and the linker will then collect them into a
+// single `rktest$data` fragment in the binary.
 //
-// This trick is based on the following article by Raymond Chen:
-// "Using linker segments and __declspec(allocate(…)) to arrange data in a specific order"
+// This trick is based on the following article by Raymond Chen: "Using linker
+// segments and __declspec(allocate(…)) to arrange data in a specific order"
 // https://devblogs.microsoft.com/oldnewthing/20181107-00/?p=100155
 //
 // Example layout:
 // +--------------+-----------------+-----------+
-// | rktest$begin | init_data_begin | rktest.o |
+// | rktest$begin | test_data_begin | rktest.o |
 // +--------------+-----------------+----------+
 // |              | test_1_case_1   | test1.o  |
 // | rktest$data  | test_1_case_2   | test1.o  |
 // |              | test_2_case_1   | test2.o  |
 // +--------------+-----------------+----------+
-// | rktest$end   | init_data_end   | rktest.o |
+// | rktest$end   | test_data_end   | rktest.o |
 // +--------------+-----------------+----------+
 #pragma section("rktest$begin", read)
 #pragma section("rktest$data", read)
 #pragma section("rktest$end", read)
 
-// Add `_rktest_test_t` pointers to mark the begining and the end of the `rktest` section.
-// Test cases are added to `rktest$data` using the TEST() macro.
-__declspec(allocate("rktest$begin")) const _rktest_test_t* const init_data_begin = NULL;
-__declspec(allocate("rktest$end")) const _rktest_test_t* const init_data_end = NULL;
+// Add `rktest_macro_data_t` pointers to mark the begining and the end of the
+// `rktest` memory section. Test cases are added to `rktest$data` using the
+// TEST() macro.
+__declspec(allocate("rktest$begin")) const rktest_macro_data_t* const test_data_begin = NULL;
+__declspec(allocate("rktest$end")) const rktest_macro_data_t* const test_data_end = NULL;
 
 static bool g_colors_enabled = false;
 
-bool _rktest_colors_enabled(void) {
+bool rktest_colors_enabled(void) {
 	return g_colors_enabled;
 }
 
 #ifdef WIN32
-static _rktest_result_t enable_windows_virtual_terminal(void) {
+static rktest_result_t enable_windows_virtual_terminal(void) {
 	// Set output mode to handle virtual terminal sequences
 	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	if (hOut == INVALID_HANDLE_VALUE) {
-		return _RKTEST_RESULT_ERROR;
+		return RKTEST_RESULT_ERROR;
 	}
 
 	DWORD dwMode = 0;
 	if (!GetConsoleMode(hOut, &dwMode)) {
-		return _RKTEST_RESULT_ERROR;
+		return RKTEST_RESULT_ERROR;
 	}
 
 	dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
 	if (!SetConsoleMode(hOut, dwMode)) {
-		return _RKTEST_RESULT_ERROR;
+		return RKTEST_RESULT_ERROR;
 	}
 
-	return _RKTEST_RESULT_OK;
+	return RKTEST_RESULT_OK;
 }
 #endif // WIN32
 
-static void init_test_runner(void) {
+static void initialize(void) {
 	g_colors_enabled = true;
 #ifdef WIN32
-	if (enable_windows_virtual_terminal() != _RKTEST_RESULT_OK) {
+	if (enable_windows_virtual_terminal() != RKTEST_RESULT_OK) {
 		fprintf(stderr, "Error: could not initialize color output\n");
 		g_colors_enabled = false;
 	}
 #endif // WIN32
 }
 
-static void run_test(const _rktest_test_t* test) {
-	_rktest_printf_green("[ RUN      ] ");
-	printf("%s.%s \n", test->suite, test->name);
+static rktest_suite_t* find_suite(rktest_suite_t* suites, size_t num_suites, const char* suite_name) {
+	foreach (rktest_suite_t*, suite, suites, num_suites) {
+		if (strcmp(suite->name, suite_name) == 0) {
+			return suite;
+		}
+	}
+	return NULL;
+}
+
+// Loop through the entirety of the `rkdata` memory section, including padding.
+// If the iterator `it` points to null, it's padding and we skip it.
+// If it's non-null, we have a test and push it into `tests`.
+static size_t register_tests(rktest_suite_t* suites) {
+	size_t num_suites = 0;
+	for (const rktest_macro_data_t* const* it = (&test_data_begin + 1); it != &test_data_end; it++) {
+		if (*it) {
+			if (num_suites == RKTEST_MAX_NUM_TEST_SUITES) {
+				fprintf(stderr, "Error: number of test suites is greater than RKTEST_MAX_NUM_TEST_SUITES. See `Config variables` section of rktest.h\n");
+				exit(1);
+			}
+
+			const rktest_macro_data_t* const data = *it;
+			rktest_suite_t* suite = find_suite(suites, num_suites, data->suite_name);
+			if (!suite) {
+				// add new suite
+				suite = &suites[num_suites++];
+				*suite = (rktest_suite_t) {
+					.name = data->suite_name,
+					.num_tests = 0,
+					.tests = { 0 },
+				};
+			}
+			// add test to suite
+			rktest_test_t* test = &suite->tests[suite->num_tests++];
+			*test = (rktest_test_t) {
+				.name = data->test_name,
+				.func = data->func,
+			};
+		}
+	}
+	return num_suites;
+}
+
+static void run_test(const rktest_test_t* test, const char* suite_name) {
+	rktest_printf_test_step("[ RUN      ] ", "%s.%s \n", suite_name, test->name);
 	if (test->func) {
 		test->func();
 	}
-	_rktest_printf_green("[       OK ] ");
-	printf("%s.%s \n", test->suite, test->name);
+	rktest_printf_test_step("[       OK ] ", "%s.%s \n", suite_name, test->name);
 }
 
-static int run_all_tests() {
-	int num_tests = 0;
-	for (const _rktest_test_t* const* it = (&init_data_begin + 1); it != &init_data_end; it++) {
-		if (*it) {
+static size_t run_all_tests(rktest_suite_t* suites, size_t num_suites) {
+	size_t num_tests = 0;
+	foreach (rktest_suite_t*, suite, suites, num_suites) {
+		foreach (rktest_test_t*, test, suite->tests, suite->num_tests) {
+			run_test(test, suite->name);
 			num_tests++;
-			run_test(*it);
 		}
 	}
 	return num_tests;
 }
 
 int rktest_main(void) {
-	init_test_runner();
+	initialize();
 
-	_rktest_printf_green("[==========] ");
-	printf("Running tests.\n");
-	_rktest_printf_green("[----------] ");
-	printf("Global test environment set-up.\n");
+	rktest_suite_t* test_suites = malloc(RKTEST_MAX_NUM_TEST_SUITES * sizeof(rktest_suite_t));
+	if (!test_suites) {
+		fprintf(stderr, "Error: could not allocate space for test data");
+	}
 
-	const int num_tests = run_all_tests();
+	const size_t num_test_suites = register_tests(test_suites);
+
+	rktest_printf_test_step("[==========] ", "Running tests.\n");
+	rktest_printf_test_step("[----------] ", "Global test environment set-up.\n");
+
+	const size_t num_tests = run_all_tests(test_suites, num_test_suites);
 
 	printf("\n");
-	_rktest_printf_green("[----------] ");
-	printf("Global test environment tear-down.\n");
-	_rktest_printf_green("[==========] ");
-	printf("%d tests from 1 test suite ran.\n", num_tests);
+	rktest_printf_test_step("[----------] ", "Global test environment tear-down.\n");
+	rktest_printf_test_step("[==========] ", "%zu tests from %zu test suite ran.\n", num_tests, num_test_suites);
 
+	free(test_suites);
 	return 0;
 }
