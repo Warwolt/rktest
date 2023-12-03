7429c532ce335f2a95f6f5c55d90440d97a07637
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-04 00:05:58 +0100

    Rename macros

--- src/rktest_poc.h
+++ src/rktest_poc.h
@@ -16,28 +16,28 @@ typedef struct {
 		.test_name = #NAME,                                                 \
 		.run = &SUITE##_##NAME##_impl,                                      \
 	};                                                                      \
-	DEFINE_SECTION_BEGIN                                                    \
+	ADD_TO_MEMORY_SECTION_BEGIN                                             \
 	const test_data_t* const SUITE##_##NAME##_ptr = &SUITE##_##NAME##_data; \
-	DEFINE_SECTION_END                                                      \
+	ADD_TO_MEMORY_SECTION_END                                               \
 	void SUITE##_##NAME##_impl(void)
 
 #if defined(_MSC_VER)
-#define DEFINE_SECTION_BEGIN                \
+#define ADD_TO_MEMORY_SECTION_BEGIN         \
 	__pragma(data_seg(push));               \
 	__pragma(section("rktest$data", read)); \
 	__declspec(allocate("rktest$data"))
 #elif defined(__APPLE__)
-#define DEFINE_SECTION_BEGIN __attribute__((used, section("__DATA,rktest")))
+#define ADD_TO_MEMORY_SECTION_BEGIN __attribute__((used, section("__DATA,rktest")))
 #elif defined(__unix__)
-#define DEFINE_SECTION_BEGIN __attribute__((used, section("rktest")))
+#define ADD_TO_MEMORY_SECTION_BEGIN __attribute__((used, section("rktest")))
 #endif
 
 #if defined(_MSC_VER)
-#define DEFINE_SECTION_END __pragma(data_seg(pop));
+#define ADD_TO_MEMORY_SECTION_END __pragma(data_seg(pop));
 #elif defined(__APPLE__)
-#define DEFINE_SECTION_END
+#define ADD_TO_MEMORY_SECTION_END
 #elif defined(__unix__)
-#define DEFINE_SECTION_END
+#define ADD_TO_MEMORY_SECTION_END
 #endif
 
 #endif /* RKTEST_POC_H */
