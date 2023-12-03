760a4256f752a3c7b1a5a43717b0e1e468118f8a
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-03 23:25:03 +0100

    Implement basic TEST macro

--- src/main.c
+++ src/main.c
@@ -11,8 +11,10 @@
 */
 
 typedef struct {
-	int value;
-} foo_t;
+	const char* suite_name;
+	const char* test_name;
+	void (*run)(void);
+} test_data_t;
 
 /* Implementation Details                                                     */
 /* -------------------------------------------------------------------------- */
@@ -22,31 +24,31 @@ __pragma(section("bar$a", read));
 __pragma(section("bar$b", read));
 __pragma(section("bar$c", read));
 
-__declspec(allocate("bar$a")) extern const foo_t* const bar_begin = NULL;
-__declspec(allocate("bar$c")) extern const foo_t* const bar_end = NULL;
+__declspec(allocate("bar$a")) extern const test_data_t* const bar_begin = NULL;
+__declspec(allocate("bar$c")) extern const test_data_t* const bar_end = NULL;
 
 #define DEFINE_SECTION \
 	__declspec(allocate("bar$b"))
 #elif defined(__APPLE__)
-extern const foo_t* const
+extern const test_data_t* const
 	__start_bar __asm("section$start$__DATA$bar");
-extern const foo_t* const
+extern const test_data_t* const
 	__stop_bar __asm("section$end$__DATA$bar");
 
 #define DEFINE_SECTION \
 	__attribute__((used, section("__DATA,bar")))
 
 DEFINE_SECTION
-const foo_t* const dummy = NULL;
+const test_data_t* const dummy = NULL;
 #elif defined(__unix__)
-extern const foo_t* const __start_bar;
-extern const foo_t* const __stop_bar;
+extern const test_data_t* const __start_bar;
+extern const test_data_t* const __stop_bar;
 
 #define DEFINE_SECTION \
 	__attribute__((used, section("bar")))
 
 DEFINE_SECTION
-const foo_t* const dummy = NULL;
+const test_data_t* const dummy = NULL;
 #endif
 
 /* Public API                                                                 */
@@ -64,24 +66,40 @@ const foo_t* const dummy = NULL;
 	(&__stop_bar)
 #endif
 
-#define REGISTER_FOO(id, value)        \
-	static const foo_t id = { value }; \
-	DEFINE_SECTION                     \
-	const foo_t* const id##_ptr = &id
+#define TEST(SUITE, NAME)                                                   \
+	void SUITE##_##NAME##_impl(void);                                       \
+	static const test_data_t SUITE##_##NAME##_data = {                      \
+		.suite_name = #SUITE,                                               \
+		.test_name = #NAME,                                                 \
+		.run = &SUITE##_##NAME##_impl,                                      \
+	};                                                                      \
+	DEFINE_SECTION                                                          \
+	const test_data_t* const SUITE##_##NAME##_ptr = &SUITE##_##NAME##_data; \
+	void SUITE##_##NAME##_impl(void)
 
 /* Usage                                                                      */
 /* -------------------------------------------------------------------------- */
 
-REGISTER_FOO(b, 234);
-REGISTER_FOO(a, 123);
-REGISTER_FOO(c, 345);
+TEST(hello, alice) {
+	printf("Hello Alice\n");
+}
+
+TEST(hello, bob) {
+	printf("Hello Bob\n");
+}
+
+TEST(hello, charlie) {
+	printf("Hello Charlie\n");
+}
 
 int main(void) {
-	const foo_t* const* it;
+	const test_data_t* const* it;
 
 	for (it = TEST_DATA_BEGIN; it < TEST_DATA_END; ++it) {
 		if (*it) {
-			printf("%d\n", (*it)->value);
+			printf("[ RUN      ] %s.%s\n", (*it)->suite_name, (*it)->test_name);
+			(*it)->run();
+			printf("[       OK ] %s.%s\n", (*it)->suite_name, (*it)->test_name);
 		}
 	}
 
