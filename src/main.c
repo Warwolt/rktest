2fdd77d150e944b5126d31bf9b25a63a2a112bc2
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-03 23:48:15 +0100

    Split macro defs into 2 groups

--- src/main.c
+++ src/main.c
@@ -25,40 +25,33 @@ __pragma(section("rktest$data", read));
 __pragma(section("rktest$end", read));
 __declspec(allocate("rktest$begin")) extern const test_data_t* const rktest_begin = NULL;
 __declspec(allocate("rktest$end")) extern const test_data_t* const rktest_end = NULL;
-
-#define DEFINE_SECTION \
-	__declspec(allocate("rktest$data"))
-
 #elif defined(__APPLE__)
 extern const test_data_t* const __start_rktest __asm("section$start$__DATA$rktest");
 extern const test_data_t* const __stop_rktest __asm("section$end$__DATA$rktest");
 __attribute__((used, section("__DATA,rktest"))) const test_data_t* const dummy = NULL;
-
-#define DEFINE_SECTION \
-	__attribute__((used, section("__DATA,rktest")))
-
 #elif defined(__unix__)
 extern const test_data_t* const __start_rktest;
 extern const test_data_t* const __stop_rktest;
 __attribute__((used, section("rktest"))) const test_data_t* const dummy = NULL;
+#endif
 
-#define DEFINE_SECTION \
-	__attribute__((used, section("rktest")))
+#if defined(_MSC_VER)
+#define DEFINE_SECTION __declspec(allocate("rktest$data"))
+#elif defined(__APPLE__)
+#define DEFINE_SECTION __attribute__((used, section("__DATA,rktest")))
+#elif defined(__unix__)
+#define DEFINE_SECTION __attribute__((used, section("rktest")))
 #endif
 
 /* Public API                                                                 */
 /* -------------------------------------------------------------------------- */
 
 #if defined(_MSC_VER)
-#define TEST_DATA_BEGIN \
-	(&rktest_begin + 1)
-#define TEST_DATA_END \
-	(&rktest_end)
+#define TEST_DATA_BEGIN (&rktest_begin + 1)
+#define TEST_DATA_END (&rktest_end)
 #elif defined(__unix__) || defined(__APPLE__)
-#define TEST_DATA_BEGIN \
-	(&__start_rktest)
-#define TEST_DATA_END \
-	(&__stop_rktest)
+#define TEST_DATA_BEGIN (&__start_rktest)
+#define TEST_DATA_END (&__stop_rktest)
 #endif
 
 #define TEST(SUITE, NAME)                                                   \
