4365438d88c60846927640c5bf558c79857a8fb5
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-04 22:46:12 +0100

    Add more char

--- tests/numeric_tests.c
+++ tests/numeric_tests.c
@@ -34,41 +34,51 @@ TEST(numeric_tests, expect_equal) {
 
 TEST(numeric_tests, expect_not_equal) {
 	EXPECT_NE(int_sum1, int_sum2);
+	EXPECT_NE(char_sum1, char_sum2);
 	EXPECT_LONG_NE(long_sum1, long_sum2);
 
 	EXPECT_NE_INFO(int_sum1, int_sum2, "int_sum2 = %d\n", int_sum2);
+	EXPECT_NE_INFO(char_sum1, char_sum2, "char_sum2 = %d\n", char_sum2);
 	EXPECT_LONG_NE_INFO(long_sum1, long_sum2, "long_sum2 = %ld\n", long_sum2);
 }
 
 /* Comparison checks */
 TEST(numeric_tests, expect_less_than) {
 	EXPECT_LT(int_sum1, int_sum2);
+	EXPECT_LT(char_sum1, char_sum2);
 	EXPECT_LONG_LT(long_sum1, long_sum2);
 
 	EXPECT_LT_INFO(int_sum1, int_sum2, "int_sum2 = %d\n", int_sum2);
+	EXPECT_LT_INFO(char_sum1, char_sum2, "char_sum2 = %d\n", char_sum2);
 	EXPECT_LONG_LT_INFO(long_sum1, long_sum2, "long_sum2 = %ld\n", long_sum2);
 }
 
 TEST(numeric_tests, expect_less_than_equal) {
 	EXPECT_LE(int_sum1, int_sum2);
+	EXPECT_LE(char_sum1, char_sum2);
 	EXPECT_LONG_LE(long_sum1, long_sum2);
 
 	EXPECT_LE_INFO(int_sum1, int_sum2, "int_sum2 = %d\n", int_sum2);
+	EXPECT_LE_INFO(char_sum1, char_sum2, "char_sum2 = %d\n", char_sum2);
 	EXPECT_LONG_LE_INFO(long_sum1, long_sum2, "long_sum2 = %ld\n", long_sum2);
 }
 
 TEST(numeric_tests, expect_greater_than) {
 	EXPECT_GT(int_sum2, int_sum1);
+	EXPECT_GT(char_sum2, char_sum1);
 	EXPECT_LONG_GT(long_sum2, long_sum1);
 
 	EXPECT_GT_INFO(int_sum2, int_sum1, "int_sum2 = %d\n", int_sum2);
+	EXPECT_GT_INFO(char_sum2, char_sum1, "char_sum2 = %d\n", char_sum2);
 	EXPECT_LONG_GT_INFO(long_sum2, long_sum1, "long_sum2 = %ld\n", long_sum2);
 }
 
 TEST(numeric_tests, expect_greater_than_equal) {
 	EXPECT_GE(int_sum2, int_sum1);
+	EXPECT_GE(char_sum2, char_sum1);
 	EXPECT_LONG_GE(long_sum2, long_sum1);
 
 	EXPECT_GE_INFO(int_sum2, int_sum1, "int_sum2 = %d\n", int_sum2);
+	EXPECT_GE_INFO(char_sum2, char_sum1, "char_sum2 = %d\n", char_sum2);
 	EXPECT_LONG_GE_INFO(long_sum2, long_sum1, "long_sum2 = %ld\n", long_sum2);
 }
