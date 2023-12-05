a2366f111f929ad6517acbee6847d00a9e3fee6b
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-05 23:47:09 +0100

    ...

--- tests/float_tests.c
+++ tests/float_tests.c
@@ -3,9 +3,9 @@
 const float float_sum1 = 0.3f + 0.3f + 0.3f;
 
 TEST(float_tests, hello) {
+	ASSERT_FLOAT_EQ_INFO(float_sum1, 0.9f, "float_sum1 = %f\n", float_sum1);
 	ASSERT_FLOAT_EQ(float_sum1, 0.9f);
-	ASSERT_FLOAT_EQ_INFO(float_sum1, 0.9f, "float_sum1 = %f", float_sum1);
 
 	EXPECT_FLOAT_EQ(float_sum1, 0.9f);
-	EXPECT_FLOAT_EQ_INFO(float_sum1, 0.9f, "float_sum1 = %f", float_sum1);
+	EXPECT_FLOAT_EQ_INFO(float_sum1, 0.9f, "float_sum1 = %f\n", float_sum1);
 }
