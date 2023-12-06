d74720c890701b96ba07d5a38c62a794a60032e0
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-06 12:00:30 +0100

    Remove sleep

--- src/rktest.c
+++ src/rktest.c
@@ -38,10 +38,6 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <time.h>
 #endif
 
-#ifndef _MSC_VER
-#include <unistd.h> // DEBUGGING
-#endif
-
 #ifdef __GNUC__
 #pragma GCC diagnostic ignored "-Wmissing-braces"
 #endif
@@ -415,11 +411,6 @@ int rktest_main(int argc, const char* argv[]) {
 	rktest_log_info("[----------] ", "Global test environment set-up.\n");
 
 	rktest_timer_t test_timer = rktest_timer_start();
-#ifdef _MSC_VER
-	Sleep(5000); // sleep 10 seconds to verify timer
-#else
-	usleep(5000 * 1000);
-#endif
 	rktest_report_t* report = run_all_tests(env);
 	rktest_millis_t test_time_ms = rktest_timer_stop(&test_timer);
 
