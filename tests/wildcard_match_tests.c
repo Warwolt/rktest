8c42af0826b9837d8ed5bc71254c20dbc1f0250b
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-09 18:40:36 +0100

    literal match

--- tests/wildcard_match_tests.c
+++ tests/wildcard_match_tests.c
@@ -5,10 +5,16 @@ static bool string_wildcard_match(const char* str, const char* pattern) {
 		return *pattern == '\0';
 	}
 
-	return false;
+	return strcmp(str, pattern) == 0;
 }
 
 TEST(wildcard_match_tests, empty_pattern_matches_only_empty_string) {
 	EXPECT_TRUE(string_wildcard_match("", ""));
 	EXPECT_FALSE(string_wildcard_match("strawberry", ""));
+}
+
+TEST(wildcard_match_tests, literal_pattern_matches_only_exact_literal) {
+	EXPECT_TRUE(string_wildcard_match("strawberry", "strawberry"));
+	EXPECT_FALSE(string_wildcard_match("straw", "strawberry"));
+	EXPECT_FALSE(string_wildcard_match("berry", "strawberry"));
 }
