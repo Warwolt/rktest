2ae9319bfdfafe37d41d039aad336735fb21e4c5
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-10 16:09:37 +0100

    Remove color from snapshot

--- tests/snapshot_test.py
+++ tests/snapshot_test.py
@@ -5,7 +5,7 @@ TEST_EXECUTABLE = './build/Debug/tests' if os.name == 'nt' else './build/tests'
 
 
 def run_test_exe(args: [str] = []) -> str:
-    result = subprocess.run([TEST_EXECUTABLE, '--rktest_print_time=0'] + args, stdout=subprocess.PIPE)
+    result = subprocess.run([TEST_EXECUTABLE, '--rktest_print_time=0', '--rktest_color=no'] + args, stdout=subprocess.PIPE)
     return result.stdout.decode('utf-8')
 
 
