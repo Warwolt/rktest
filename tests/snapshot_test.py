989627610d628f1a06edcceb392ffabba5468340
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2023-12-10 17:05:16 +0100

    Update snapshots with failing integer tests

--- tests/snapshot_test.py
+++ tests/snapshot_test.py
@@ -2,33 +2,38 @@ import os
 import subprocess
 
 TEST_EXECUTABLE = './build/Debug/tests' if os.name == 'nt' else './build/tests'
+FAILING_TEST_EXECUTABLE = './build/Debug/failing_tests' if os.name == 'nt' else './build/failing_tests'
 
 
-def run_test_exe(args: [str] = []) -> str:
-    result = subprocess.run([TEST_EXECUTABLE, '--rktest_print_time=0',
+def run_test_exe(exe, args: [str] = []) -> str:
+    result = subprocess.run([exe, '--rktest_print_time=0',
                             '--rktest_color=no'] + args, stdout=subprocess.PIPE)
     return result.stdout.decode('utf-8')
 
 
 def test_no_args(snapshot):
-    actual = run_test_exe()
+    actual = run_test_exe(TEST_EXECUTABLE)
     assert actual == snapshot
 
 
 def test_wildcard_match(snapshot):
-    actual = run_test_exe(['--rktest_filter=*'])
+    actual = run_test_exe(TEST_EXECUTABLE, ['--rktest_filter=*'])
     assert actual == snapshot
 
 
 def test_suffix_match(snapshot):
-    actual = run_test_exe(['--rktest_filter=*equal'])
+    actual = run_test_exe(TEST_EXECUTABLE, ['--rktest_filter=*equal'])
     assert actual == snapshot
 
 
 def test_prefix_match(snapshot):
-    actual = run_test_exe(['--rktest_filter=integer*'])
+    actual = run_test_exe(TEST_EXECUTABLE, ['--rktest_filter=integer*'])
     assert actual == snapshot
 
 def test_infix_match(snapshot):
-    actual = run_test_exe(['--rktest_filter=*tests*'])
+    actual = run_test_exe(TEST_EXECUTABLE, ['--rktest_filter=*tests*'])
+    assert actual == snapshot
+
+def test_failing_tests(snapshot):
+    actual = run_test_exe(FAILING_TEST_EXECUTABLE)
     assert actual == snapshot
