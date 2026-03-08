520ac769dea2d0adaa0f179065fc80d7f2e45398
Rasmus Källqvist <rkallqvist@microsoft.com>
2026-03-08 22:11:12 +0100

    snaps

--- tests/death_tests.c
+++ tests/death_tests.c
@@ -5,21 +5,21 @@
 
 #ifndef RKTEST_FAILING_TESTS
 static const int exit_code = 1;
-static const char* expected_stderr1 = "Error!";
-static const char* expected_stderr2 = "Error?";
+static const char* expected_stderr1 = "Hello World";
+static const char* expected_stderr2 = "Foo Bar";
 #else
 static const int exit_code = 0;
-static const char* expected_stderr1 = "Error!!";
-static const char* expected_stderr2 = "Error??";
+static const char* expected_stderr1 = "Hello Moon";
+static const char* expected_stderr2 = "Bar Baz";
 #endif
 
 TEST(death_tests, expect_death) {
 	ASSERT_DEATH({
-		fprintf(stderr, "Error!\n");
+		fprintf(stderr, "Hello World\n");
 		exit(exit_code);
 	}, expected_stderr1);
 	EXPECT_DEATH({
-		fprintf(stderr, "Error?\n");
+		fprintf(stderr, "Foo Bar\n");
 		exit(exit_code);
 	}, expected_stderr2);
 }
